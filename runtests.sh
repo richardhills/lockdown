95c0a65e9d5fdaa8419f9edaae6356d84eb6e442
Richard Hills <richard.hills@10to8.com>
2021-09-02 22:25:03 +0100

    Fixes for python3

--- runtests.sh
+++ runtests.sh
@@ -4,14 +4,14 @@ export PYTHONPATH=.
 #python lockdown/test.py -t
 #echo "FINISHED NO RTTI TEST - FASTEST"
 echo "-----------------------------------------"
-echo "STARTING NORMAL MODE TESTS"
-python lockdown/test.py
-echo "FINISHED NORMAL MODE TESTS"
+echo "STARTING FAST MODE TESTS"
+python lockdown/test.py -fopT
+echo "FINISHED FAST MODE TESTS"
 echo "-----------------------------------------"
 #echo "STARTING DEBUG MODE, NO RTTI TESTS"
 #python lockdown/test.py -dt
 #echo "FINISHED DEBUG MODE, NO RTTI TESTS"
 echo "-----------------------------------------"
-echo "STARTING DEBUG MODE TESTS"
-python lockdown/test.py -d
-echo "FINISHED DEBUG MODE TESTS"
+echo "STARTING STRICT MODE TESTS"
+python lockdown/test.py -spr
+echo "FINISHED STRICT MODE TESTS"
